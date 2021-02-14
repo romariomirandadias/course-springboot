a759b728aaf7918f37b8fdc039636e72055d2de6
romariomirandadias <rmirandadias@hotmail.com>
2021-02-13 23:25:32 -0300

    Entidade Pedido.Datas com Instant e padrão ISO 8601-PARTE 1

---
+++ src/main/java/com/educandoWeb/course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.educandoWeb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoWeb.course.entities.User;
+import com.educandoWeb.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
