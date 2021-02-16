25de756f9ad9752a3f04330e6d39b9dba04b8a30
romariomirandadias <rmirandadias@hotmail.com>
2021-02-16 08:26:12 -0300

    Product

--- src/main/java/com/educandoWeb/course/resources/OrderResources.java
+++ src/main/java/com/educandoWeb/course/resources/OrderResources.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class OrderResources {
 		return ResponseEntity.ok().body(list);
 	}
 @GetMapping(value="/{id}")
-	public ResponseEntity<Order> findById(Long id) {
+	public ResponseEntity<Order> findById(@PathVariable Long id) {
 		Order obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
