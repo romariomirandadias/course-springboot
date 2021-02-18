1535aa4b33c36fc2b3137edd6b338a995a6415e8
romariomirandadias <rmirandadias@hotmail.com>
2021-02-17 23:32:26 -0300

    Tratamento de exceção com findById

---
+++ src/main/java/com/educandoWeb/course/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.educandoWeb.course.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
+}
